95271237d15110c7aaa942fe990243326cf04dc5
Walid Sboui <walid.sboui@insat.ucar.tn>
2022-12-19 15:18:39 +0100

    Add project files.

---
+++ TP4/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP4.Data;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ActionResult Index()
+        {
+
+
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            StudentRepository studentRepository = new StudentRepository(universityContext);
+            foreach (String s in studentRepository.GetCourses())
+                Debug.WriteLine(s);
+
+            return View(studentRepository.GetCourses());
+        }
+
+        public ActionResult GetCourse(string id)
+        {
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            StudentRepository studentRepository = new StudentRepository(universityContext);
+            IEnumerable<Student> res = (IEnumerable<Student>)studentRepository.Find(v => v.course.ToLower() == id.ToLower());
+            //foreach (Student s in res)
+            //{
+            //    Debug.WriteLine(s.id);
+            //} 
+
+            if (res.Count() != 0) ViewBag.Id = res.First().course;
+            return View(res);
+        }
+    }
+}
